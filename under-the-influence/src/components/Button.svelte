cfce33c0409ef4bd67e2a08473aaccbbf9011550
MBlazina <mirko.blazina@gmail.com>
2022-03-31 02:35:57 +0200

    first screen

---
+++ under-the-influence/src/components/Button.svelte
@@ -0,0 +1,36 @@
+<script>
+	export let text;
+	export let type;
+	export let link;
+</script>
+
+<a class={type} href={link}>{text}</a>
+
+<style>
+	a {
+		border: none;
+		width: 422px;
+		height: 64px;
+		background: #ffffff;
+		border-radius: 12px;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		text-decoration: none;
+		font-family: 'Roboto';
+		font-style: normal;
+		font-weight: 900;
+		font-size: 18px;
+		letter-spacing: -0.07em;
+		color: #000000;
+		font-family: 'Roboto', sans-serif;
+	}
+	.white {
+		background: #ffffff;
+		color: #000000;
+	}
+	.grey {
+		background: #000;
+		color: #fff;
+	}
+</style>
--- under-the-influence/src/routes/index.svelte
+++ under-the-influence/src/routes/index.svelte
@@ -1,4 +1,55 @@
+<script context="module">
+	export async function load({ fetch }) {
+		const url = import.meta.env.VITE_ENDPOINT;
+		const response = await fetch(url);
+		const result = await response.json();
+
+		return {
+			props: {
+				result: result[0].title.rendered,
+				result1: result[1].title.rendered,
+				abc: 3
+			}
+		};
+	}
+</script>
+
+<script>
+import Button from "../../src/components/Button.svelte";
+
+	export let result;
+	export let result1;
+	export let abc;
+
+	let a = abc + 100;
+</script>
+<!-- <h1>{a}</h1>
+	<h3>{import.meta.env.VITE_ENDPOINT}</h3>
+	
+	<p>{result}</p>
+	<p>{result1}</p> -->
+<main>
+	<h1>Under the influence</h1>
+	<img src="logo.svg" alt="under the influence logo">
+	<Button	text="START PLAYING" type="white" link="/choose-game"/>
+</main>
 
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<style>
+h1{
+	display: none;
+}
+img {
+	margin-bottom: 96px;
+}
+main {
+		width: 100%;
+		height: 100vh;
+		background: linear-gradient(
+			123.35deg, #EBF3D0 0%, rgba(235, 243, 208, 0) 18.4%), radial-gradient(29.9% 70.94% at 44.25% 86.96%, #DC8DDC 0%, rgba(220, 141, 220, 0) 100%) /* warning: gradient uses a rotation that is not supported by CSS and may not behave as expected */, radial-gradient(33.83% 53.57% at 35.87% 100%, #DC8DDC 0%, rgba(220, 141, 220, 0) 100%) /* warning: gradient uses a rotation that is not supported by CSS and may not behave as expected */, radial-gradient(42.66% 49.72% at 45.56% 44.65%, #CBADEB 0%, rgba(194, 166, 241, 0) 100%) /* warning: gradient uses a rotation that is not supported by CSS and may not behave as expected */, linear-gradient(134.59deg, #CDF9E8 20.63%, rgba(205, 249, 232, 0) 47.84%), linear-gradient(216.44deg, rgba(192, 169, 240, 0) -16.52%, #C0A9F0 -1.04%, rgba(192, 169, 240, 0) 16.99%), linear-gradient(128.53deg, rgba(192, 169, 240, 0) 28.63%, #C0A9F0 38.5%, rgba(192, 169, 240, 0) 50.26%), radial-gradient(40.75% 97.37% at 90.75% 40.15%, #FFFDB1 0%, #FEE4BF 34.46%, #F0BDD0 69.5%, rgba(255, 129, 38, 0) 100%) /* warning: gradient uses a rotation that is not supported by CSS and may not behave as expected */, #C2A6F1;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex-direction: column;
+	} 
+</style>
