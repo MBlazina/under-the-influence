cfe19e11f6a3342039d817568efc1c3630b4e4a5
MBlazina <mirko.blazina@gmail.com>
2022-04-02 04:29:28 +0200

    add player functionality

--- under-the-influence/src/routes/add-player.svelte
+++ under-the-influence/src/routes/add-player.svelte
@@ -2,14 +2,63 @@
 	import Button from '../components/Button.svelte';
 	import Logo from '../components/Logo.svelte';
 	import Header from '../components/Header.svelte';
+	import { onMount } from 'svelte';
+	import { name } from '../stores/players.js';
+
+	let numPlayers = 1;
+
+	function addPlaceholder() {
+		let playerInputs = document.querySelectorAll('.inputWrapper > input');
+		let numPlayers = playerInputs.length;
+		for (let i = 0; i < numPlayers; i++) {
+			playerInputs[i].setAttribute('placeholder', 'Player ' + (i + 1));
+		}
+	}
+
+	function handleAppend() {
+		let players = document.querySelector('.players-form');
+		let currentPlayers = document.querySelectorAll('.inputWrapper').length;
+		console.log(currentPlayers);
+
+		let inputWrapper = document.querySelector('.inputWrapper');
+		let newInput = inputWrapper.cloneNode(true);
+
+		players.append(newInput);
+		addPlaceholder();
+
+		addRemove();
+	}
+
+	function removePlayer() {
+		console.log(this.parentElement);
+		this.parentElement.remove();
+		addPlaceholder();
+	}
+	function addRemove() {
+		let removeIcons = document.querySelectorAll('.removeIcon');
+		for (let i = 0; i < removeIcons.length; i++) {
+			removeIcons[i].addEventListener('click', removePlayer);
+		}
+	}
+	onMount(() => {});
 </script>
 
 <svelte:head>
 	<title>Add players</title>
 </svelte:head>
+
 <main>
 	<Header>
 		<Logo />
 	</Header>
 	<h2>Add players</h2>
+	<span>{numPlayers}</span>
+	<form on:submit|preventDefault class="players-form" action="">
+		<div class="inputWrapper">
+			<input type="text" placeholder="Player 1" />
+			<div class="removeIcon">X</div>
+		</div>
+	</form>
+	<input type="submit" />
+	<button on:click={handleAppend}>Add player</button>
 </main>
