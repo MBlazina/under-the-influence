7f74f2fc9314f3ffcfc7d6b940272af64d45dd1e
MBlazina <mirko.blazina@gmail.com>
2022-07-25 22:48:09 +0200

    add/remove players inputs

--- under-the-influence/src/routes/add-player.svelte
+++ under-the-influence/src/routes/add-player.svelte
@@ -6,117 +6,35 @@
 	import { goto } from '$app/navigation';
 	import { query_selector_all } from 'svelte/internal';
 
-	let numPlayers = 1;
-
-/* 	function addPlaceholder() {
-		let playerInputs = document.querySelectorAll('.inputWrapper > input');
-		let numPlayers = playerInputs.length;
-		for (let i = 0; i < numPlayers; i++) {
-			playerInputs[i].setAttribute('placeholder', 'Player ' + (i + 1));
+	$: tempPlayers = [
+		{
+			id: 1,
+			name: 'Player 1'
+		},
+		{
+			id: 2,
+			name: 'Player 2'
 		}
-	} */
-
-/* 	function handleAppend() {
-		let players = document.querySelector('.players-form');
-		let inputWrapper = document.querySelector('.inputWrapper');
-		let newInput = inputWrapper.cloneNode(true);
-		newInput.querySelector('input').value = '';
-
-		players.append(newInput);
-		addPlaceholder();
-		addRemove();
-	} */
-	function handleValidate(el) {
-		/* const inputValue = el.target.value; */
-		const arr = document.querySelectorAll('input');
-		const inputs = arr.length;
-
-		for (let i = 0; i < inputs; i++) {
-			const currentInput = arr[i];
-			const inputValue = arr[i].value;
-
-			let arrSum = 0;
-			for (let j = 0; j < inputs; j++) {
-				if (inputValue === arr[j].value) {
-					arrSum += 1;
-				}
-			}
-			if (arrSum > 1 || inputValue == '') {
-				currentInput.classList.add('duplicate');
-			} else {
-				currentInput.classList.remove('duplicate');
-			}
-		}
-		const isValid = document.querySelectorAll('input.duplicate').length === 0;
-		if(isValid) {
-			$playersValid = true;
-		}
-		else {
-			$playersValid = false;
-		}
-		console.log('players valid: ' + $playersValid);
-		
-	}
-	/* function removePlayer() {
-		let currentPlayersNumber = document.querySelectorAll('.inputWrapper').length;
-		if (currentPlayersNumber > 1) {
-			this.parentElement.remove();
-			addPlaceholder();
-		}
-	}
-	function addRemove() {
-		let removeIcons = document.querySelectorAll('.removeIcon');
-		for (let i = 0; i < removeIcons.length; i++) {
-			removeIcons[i].addEventListener('click', removePlayer);
-		}
-	} */
-
-
-
-	//TEST
-	
-	function updatePlayers() {
-		$players = [];
-		const tempPlayers = document.querySelectorAll('input');
-
-		for (let i = 0; i < tempPlayers.length; i++) {
-			if (tempPlayers[i].value) {
-				$players = [...$players, tempPlayers[i].value];
-			}
-		}
-
-	}
-
-function handleStartGame() {
-		updatePlayers();
-
-	}
-
-	//TEST VALIDATE
+	];
 
 	function handleAddPlayer() {
-		
-		updatePlayers();
-		$players = [...$players, 'Player ' + parseInt($players.length + 1)];
-		handleValidate();
-		
+		console.log(tempPlayers[tempPlayers.length - 1].id);
+		const lastPlayerId = tempPlayers[tempPlayers.length - 1].id;
+		const nextPlayerId = lastPlayerId + 1;
+		tempPlayers = [...tempPlayers, { id: nextPlayerId, name: 'Player ' + nextPlayerId }];
 	}
 	function handleDeletePlayer(selectedPlayer) {
-		console.log(selectedPlayer);
-		$players = $players.filter(function (player, index, arr) {
-			if (player != selectedPlayer) return player;
-		});
-		console.log($players);
-		handleValidate();
+		if (tempPlayers.length > 1) {
+			tempPlayers = tempPlayers.filter((data) => data.id != selectedPlayer);
+		}
 	}
-	//let hasDuplicatePlayers = document.querySelectorAll('input');
 </script>
 
 <svelte:head>
 	<title>Add players</title>
 	<style>
 		.duplicate {
-			color: #FF5353;
+			color: #ff5353;
 		}
 	</style>
 </svelte:head>
@@ -126,22 +44,16 @@ function handleStartGame() {
 		<Logo />
 	</Header>
 
-	<h2>Add players</h2>
-
-	{#if $players.length == 0}
-		<input type="text" placeholder="Player 1" on:input={handleValidate}/>
-		<button disabled>X</button>
-		<input type="text" placeholder="Player 2" on:input={handleValidate}/>
-		<button disabled>X</button>
-	{/if}
-
-	{#each $players as player}
-		<input type="text" value={player} on:input={handleValidate}/>
-		<button on:click={handleDeletePlayer(player)}>X</button>
+	{#each tempPlayers as player}
+		<input type="text" placeholder={player.name} bind:value={player.name} />
+		<button on:click={handleDeletePlayer(player.id)}>X</button>
 	{/each}
-	<button class="add-players-btn" on:click={handleAddPlayer}>Add a player</button>
-	<a href={$activeGame != '' ? 'game/' + $activeGame : '/choose-game'} class="start-game-btn" class:enabled="{$playersValid == true}" on:click={handleValidate}>START THE GAME</a>
-	<p>{$players}</p>
+
+	<button on:click={handleAddPlayer}>Add</button>
+
+	<h2>Add players</h2>
+	<form id="playersForm" action="" />
+	<a href="">Add Player</a>
 </main>
 
 <style lang="scss">
@@ -214,13 +126,11 @@ function handleStartGame() {
 
 		background: $grey;
 		color: $white;
-pointer-events: none;
+		pointer-events: none;
 		cursor: pointer;
-		&.enabled{
+		&.enabled {
 			pointer-events: all;
 			background: $dark-blue;
 		}
 	}
-
-
 </style>
--- under-the-influence/src/stores/players.js
+++ under-the-influence/src/stores/players.js
@@ -1,5 +1,14 @@
 import { writable } from "svelte/store";
 
-export let players = writable([]);
+export let players = writable([
+  {
+    "id": "player1",
+    "name": "Player 1"
+  },
+  {
+    "id": "player2",
+    "name": "Player 2"
+  }
+]);
 export let playersValid = writable(false);
 export let activeGame = writable('');
